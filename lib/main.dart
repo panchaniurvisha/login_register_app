51f4273311764b19f018aa8090f2252106178d90
pills <urvisha1109@gmail.com>
2023-07-27 21:59:16 +0530

    "27-7-2023"

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:login_register_app/utils/routes/routes.dart';
 import 'package:login_register_app/utils/routes/routes_name.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
